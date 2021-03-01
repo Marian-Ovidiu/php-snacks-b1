6c54db773562d78ecac586b03c68c3ced9e0f55b
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-03-01 16:20:18 +0100

    Finito snack 3

---
+++ snack3.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <?php
+    $posts = [
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+
+  ?>
+
+  <?php
+    foreach($posts as $key => $value) { ?>
+      <h3><?php echo $key ?></h3><br>
+      <?php $chiave = $value;
+      foreach ($chiave as $element) { ?>
+        <div class=""><?php echo $element['title']; ?></div>
+        <div class=""><?php echo $element['author']; ?></div>
+        <div class=""><?php echo $element['text']; ?></div>
+      <?php } ?>
+   <?php } ?>
+
+  </body>
+</html>
