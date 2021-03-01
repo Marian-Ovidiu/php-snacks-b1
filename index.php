0f0016b39d91f4b200d8531bcff4275e1a416ee0
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-03-01 15:35:03 +0100

    Finito snack 1

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-$matches = [
-  'Match1' => [
-    'Casa' => 'Atalanta',
-    'PuntiCasa' => '2',
-    'Ospite' => 'Benevento',
-    'PuntiOspite' => '0'
-  ],
-  'Match2' => [
-    'Casa' => 'Bologna',
-    'PuntiCasa' => '3',
-    'Ospite' => 'Cagliari',
-    'PuntiOspite' => '3'
-  ],
-  'Match3' => [
-    'Casa' => 'Crotone',
-    'PuntiCasa' => '3',
-    'Ospite' => 'Fiorentina',
-    'PuntiOspite' => '1'
-  ],
-  'Match4' => [
-    'Casa' => 'Genoa',
-    'PuntiCasa' => '2',
-    'Ospite' => 'Inter',
-    'PuntiOspite' => '3'
-  ],
-  'Match5' => [
-    'Casa' => 'Juventus',
-    'PuntiCasa' => '1',
-    'Ospite' => 'Lazio',
-    'PuntiOspite' => '2'
-  ]
-];
+    <?php
 
-foreach ($matches as $interno) {
+    $matches = [
+      [
+        'Casa' => 'Atalanta',
+        'PuntiCasa' => '2',
+        'Ospite' => 'Benevento',
+        'PuntiOspite' => '0'
+      ],
+      [
+        'Casa' => 'Bologna',
+        'PuntiCasa' => '3',
+        'Ospite' => 'Cagliari',
+        'PuntiOspite' => '3'
+      ],
+      [
+        'Casa' => 'Crotone',
+        'PuntiCasa' => '3',
+        'Ospite' => 'Fiorentina',
+        'PuntiOspite' => '1'
+      ],
+      [
+        'Casa' => 'Genoa',
+        'PuntiCasa' => '2',
+        'Ospite' => 'Inter',
+        'PuntiOspite' => '3'
+      ],
+      [
+        'Casa' => 'Juventus',
+        'PuntiCasa' => '1',
+        'Ospite' => 'Lazio',
+        'PuntiOspite' => '2'
+      ]
+    ];
 
-    if (is_array($interno)){
+    ?>
 
-        foreach ($interno as $valore) {
-
-            echo $valore;
-        }
-    } else {
-
-        echo $interno;
-    };
-};
+    <ul>
+      <?php
+      for ($i=0; $i < count($matches); $i++) { ?>
+        <li> <?php echo $matches[$i]['Casa'] . ' - ' . $matches[$i]['Ospite'] . ' | ' . $matches[$i]['PuntiCasa'] . ' - ' . $matches[$i]['PuntiOspite'];?></li>
+      <?php } ?>
+    </ul>
+  </body>
+</html>
